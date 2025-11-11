86e0648e2fbf1b521b8135809c2edec518757155
João Barreto <Joo.bcsilva@upe.br>
2025-11-11 17:45:39 -0300

    Criar o repositório que vai servir para armazenar os códigos do projeto final da disciplina de programação II

---
+++ src/test/java/projeto_prog_ii/ProjetoProgIiApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_prog_ii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoProgIiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
