659decc6c6476f8ffbd0a583cdb670d12bd0b0ef
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-11-16 23:01:29 -0300

    criando método de cadastro de aluno/ remoção de aluno.java, nolugar a classe java

--- src/main/java/projeto_prog_ii/model/Aluno.java
+++ src/main/java/projeto_prog_ii/model/Aluno.java
@@ -1,5 +1,26 @@
 package projeto_prog_ii.model;
 import java.util.ArrayList;
 import java.util.List;
-public class Aluno {}
+import java.util.Scanner;
+public class Aluno {
+
+    private String nome;
+    private int idade,matricula;
+    private double nota;
+
+    ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+    Scanner sc = new Scanner(System.in);
+
+    public void Cadastro(){
+        System.out.println("Digite seu nome: ");
+        nome = sc.nextLine();
+        System.out.println("Digite sua idade: ");
+        idade = sc.nextInt();
+
+
+    }
+
+
+
+}
 
