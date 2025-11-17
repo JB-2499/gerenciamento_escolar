e2cb447100c7576580b87cffeb2c80d256b6ff1a
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-11-17 19:24:34 -0300

    adicionando metodos Professor.java

--- src/main/java/projeto_prog_ii/model/Professor.java
+++ src/main/java/projeto_prog_ii/model/Professor.java
@@ -1,12 +1,19 @@
 package projeto_prog_ii.model;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Professor {
     Scanner sc = new Scanner(System.in);
     private String nome, materia;
+    private int idade;
+    private double nota1, nota2, nota3;
+
+
 
     public void CadastroProfessor(){
-
 
     }
 
