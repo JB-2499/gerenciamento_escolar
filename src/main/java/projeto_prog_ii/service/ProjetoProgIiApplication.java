b6412bf9e2aa846fc111be885df083fc9d833364
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-11-17 23:10:52 -0300

    classe professor com metodo CadastroProfessor

---
+++ src/main/java/projeto_prog_ii/service/ProjetoProgIiApplication.java
@@ -0,0 +1,13 @@
+package projeto_prog_ii.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoProgIiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoProgIiApplication.class, args);
+    }
+
+}
