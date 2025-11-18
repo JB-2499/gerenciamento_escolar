5e14e2a9dcec5005e97076b6e3cc6e0b7db031aa
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-11-17 23:35:12 -0300

    Merge branch 'Felipe' of https://github.com/JB-2499/gerenciamento_escolar into Felipe
    # Conflicts:
    #	src/main/java/projeto_prog_ii/model/Aluno.java

--- src/main/java/projeto_prog_ii/model/Professor.java
+++ src/main/java/projeto_prog_ii/model/Professor.java
@@ -7,6 +7,7 @@ public class Professor {
     ArrayList<Professor> professor = new ArrayList<Professor>();
     private String nome, materia;
     private int idade;
+    private double nota1, nota2, nota3;
 
     Professor(String nome, String materia, int idade) {
         this.nome = nome;
@@ -24,5 +25,52 @@ public class Professor {
 
         Professor p  = new Professor( nome,  materia, idade);
         professor.add(p);
+    }
+
+    public void CadastarNotas(String nomeAluno){
+
+        System.out.println("Digite a primeira nota: ");
+        nota1 = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("Digite a segunda nota: ");
+        nota2 = sc.nextDouble();
+        sc.nextLine();
+
+        double media = (nota1 + nota2) / 2;
+
+        if (media >= 7){
+             String situacao = "Aprovado";
+            Alunos.add(situacao,);
+            Alunos.add(media);
+        }
+        else if (media<7 || media>3) {
+
+            System.out.println("Digite a nota da final: ");
+            nota3 = sc.nextDouble();
+            sc.nextLine();
+
+            media = ( media + nota3 ) / 2 ;
+
+            if(media < 5){
+                String situacao = "Reprovado"
+                Alunos.add(situacao,);
+                Alunos.add(media);
+            }
+            else{
+                String situacao = "Aprovado";
+                Alunos.add(situacao,);
+                Alunos.add(media);
+            }
+
+        }
+        else{
+            String situacao = "Reprovado";
+            Alunos.add(situacao,);
+            Alunos.add(media);
+        }
+
+
+
     }
 }
