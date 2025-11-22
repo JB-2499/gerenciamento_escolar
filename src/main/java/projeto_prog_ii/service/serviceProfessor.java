e94d0be24f27ba9d8b26c8b65d684e9f7ba90f71
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-11-22 16:44:40 -0300

    após a remoção dos metodos anteriores, optimizei  o codigo e acrescentei coisas no serviceProfessor.java

--- src/main/java/projeto_prog_ii/service/serviceProfessor.java
+++ src/main/java/projeto_prog_ii/service/serviceProfessor.java
@@ -1,4 +1,15 @@
 package projeto_prog_ii.service;
 public class serviceProfessor {
 
+
+
+    public void calcularMedia(double nota1, double nota2){
+
+        double media;
+        boolean situacao;
+
+
+
+
+    }
 }
