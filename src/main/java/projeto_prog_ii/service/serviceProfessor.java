fb0da99b5f7f8d9d6ecc3ce3acfae37e940d3d36
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-11-22 16:58:07 -0300

    após a remoção dos metodos anteriores, optimizei  o codigo e acrescentei coisas no serviceProfessor.java

--- src/main/java/projeto_prog_ii/service/serviceProfessor.java
+++ src/main/java/projeto_prog_ii/service/serviceProfessor.java
@@ -8,7 +8,23 @@ public class serviceProfessor {
         double media;
         boolean situacao;
 
+        media = (nota1 + nota2) / 2;
 
+        if (media >= 3 && media <7) {
+            situacao = false;
+            if (media <5){
+                situacao = false;}
+
+            else {
+                situacao = true;
+            }
+        }
+        else if (media < 3) {
+            situacao = false;
+        }
+        else {
+            situacao = true;
+        }
 
 
     }
