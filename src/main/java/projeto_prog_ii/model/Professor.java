96b744346e6973565a35157ba0c911010495fec3
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-11-23 12:59:28 -0300

    Atualização das classes "Aluno_service" e "Aluno_controller".

--- src/main/java/projeto_prog_ii/model/Professor.java
+++ src/main/java/projeto_prog_ii/model/Professor.java
@@ -1,26 +1,25 @@
 package projeto_prog_ii.model;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Scanner;
-import lombok.Setter;
-import lombok.Getter;
 
+@Getter @Setter
 public class Professor {
 
     Scanner sc = new Scanner(System.in);
     private ArrayList<Professor> professor = new ArrayList<Professor>();
-    private String nome, materia;
+    String nome, materia;
     private int idade;
     private double nota1, nota2, nota3;
+
+    public Professor() {}
 
     Professor(String nome, String materia, int idade) {
         this.nome = nome;
         this.materia = materia;
         this.idade = idade;
     }
-
-    Professor p  = new Professor(nome,materia,idade);
-
-
-
 
 }
---
+++ src/main/java/projeto_prog_ii/repository/repositoryProfessor.java
@@ -0,0 +1,5 @@
+package projeto_prog_ii.repository;
+import org.springframework.stereotype.Repository;
+@Repository
+public class repositoryProfessor {
+}
