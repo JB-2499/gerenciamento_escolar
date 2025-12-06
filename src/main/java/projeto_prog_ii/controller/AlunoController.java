a7696edb470230bdf34571b7012d5da4f2018bf5
João Barreto <Joo.bcsilva@upe.br>
2025-12-06 17:19:12 -0300

    Atualizar nome da tabela.

--- src/main/java/projeto_prog_ii/controller/AlunoController.java
+++ src/main/java/projeto_prog_ii/controller/AlunoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/Alunos")
+@RequestMapping("/Tb_Aluno")
 public class AlunoController {
 
     private final AlunoService alunoService;
@@ -21,7 +21,7 @@ public class AlunoController {
     public ResponseEntity<Aluno> registerAluno(@Valid @RequestBody Aluno aluno) {
         Aluno novoAluno = alunoService.registerAluno(aluno);
 
-        return ResponseEntity.created(URI.create("/Alunos" + novoAluno.getId())).body(novoAluno);
+        return ResponseEntity.created(URI.create("/Tb_Aluno" + novoAluno.getId())).body(novoAluno);
     }
 
     @GetMapping("/{id}")
--- src/main/java/projeto_prog_ii/model/Aluno.java
+++ src/main/java/projeto_prog_ii/model/Aluno.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data
 @Entity
-@Table(name = "Alunos")
+@Table(name = "Tb_Aluno")
 public class Aluno {
 
     @Id
