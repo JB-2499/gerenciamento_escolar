572d9e0cf827f6ec4738afda41e6cbce012259c8
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-12-06 17:17:43 -0300

    modificação no codigo model
    # Conflicts:
    #	src/main/java/projeto_prog_ii/model/Professor.java

--- src/main/java/projeto_prog_ii/model/Professor.java
+++ src/main/java/projeto_prog_ii/model/Professor.java
@@ -1,22 +1,27 @@
 package projeto_prog_ii.model;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import jakarta.validation.constraints.*;
 
-@Getter
-@Setter
+@Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Table (name = "Professor")
+@Table (name = "Professores")
 
 public class Professor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String nome, materia;
+
+    @NotBlank(message = "O nome não pode estar vazio.")
+    private String nome;
+
+    @NotBlank(message = "A matéria não pode estar vazia.")
+    private String materia;
+
+    @Min(value = 22, message = "A idade mínima é 22.")
+    @Max(value = 99, message = "A idade máxima é 99.")
     private int idade;
 
+    @ManyToMany(mappedBy = "Professores")
+    private List<Turmas> turmas;
 }
