5b3eae336a40c3ce8e77d6eca2588f707161e36e
Ediene <edienegomes44@gmail.com>
2025-12-07 00:32:47 -0300

    Integração e importações classe Professor

---
+++ src/main/java/projeto_prog_ii/model/Professor.java
@@ -0,0 +1,31 @@
+package projeto_prog_ii.model;
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Tb_Professor")
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "O nome não pode estar vazio.")
+    private String nome;
+
+    @NotBlank(message = "A matéria não pode estar vazia.")
+    private String materia;
+
+    @Min(value = 22, message = "A idade mínima é 22.")
+    @Max(value = 99, message = "A idade máxima é 99.")
+    private int idade;
+
+    @ManyToMany(mappedBy = "professores")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Turma> turmas = new ArrayList<>();
+}
---
+++ src/main/java/projeto_prog_ii/service/ProfessorService.java
@@ -0,0 +1,40 @@
+package projeto_prog_ii.service;
+
+import org.springframework.stereotype.Service;
+import projeto_prog_ii.model.Professor;
+import projeto_prog_ii.repository.ProfessorRepository;
+import java.util.List;
+@Service
+public class ProfessorService {
+
+    private final ProfessorRepository professorRepository;
+
+    public ProfessorService(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
+
+    public Professor createProfessores(Professor professor){
+        return professorRepository.save(professor);
+    }
+    public Professor readProfessores(Long id) {
+        return professorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+    }
+    public List<Professor> readAllProfessores() {
+        return professorRepository.findAll();
+    }
+
+    public Professor updateProfessores(Long id, Professor professorAtualizado) {
+        Professor existente = professorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+
+        existente.setNome(professorAtualizado.getNome());
+        existente.setMateria(professorAtualizado.getMateria());
+        existente.setIdade(professorAtualizado.getIdade());
+
+        return professorRepository.save(existente);
+    }
+    public void delete(Long id){
+        professorRepository.deleteById(id);
+    }
+}
