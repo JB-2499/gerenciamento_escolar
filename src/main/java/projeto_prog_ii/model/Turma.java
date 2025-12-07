af6a80604707d7e59ec8414a75d28c8f76490d50
Ediene <edienegomes44@gmail.com>
2025-12-07 00:35:25 -0300

    Adição atributo e renomeção da table

--- src/main/java/projeto_prog_ii/model/Turma.java
+++ src/main/java/projeto_prog_ii/model/Turma.java
@@ -5,14 +5,14 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Entity
-@Table (name = "turma")
+@Table (name = "Tb_Turma")
 public class Turma {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,17 +21,19 @@ public class Turma {
     private String nome;
 
     @Min(value = 20, message = "A quantidade mínima é 20 alunos por turma.")
-    @Max(value = 40, message = "A quantidade máxima é 40 alunos por turma.")
+    @Max(value = 40, message = "A quantidade máxima é 40 alunos por turma.")
     private int quantiaAluno;
+    private int quantidadeProfessor;
 
     //Tipo de relacionamento com a classe professor
-    /*@ManyToMany
+    @ManyToMany
     @JoinTable(
-            name = "turma_professor",
+            name = "tb_turma_professor",
             joinColumns = @JoinColumn(name = "turma_id"),
             inverseJoinColumns = @JoinColumn(name = "professor_id")
     )
-    private List<Professor> professores;*/
+    private List<Professor> professores = new ArrayList<>();
+
     //Tipo de relacionamento com a classe aluno
     @OneToMany(mappedBy = "turma", cascade = CascadeType.ALL, orphanRemoval = true)
     @ToString.Exclude
