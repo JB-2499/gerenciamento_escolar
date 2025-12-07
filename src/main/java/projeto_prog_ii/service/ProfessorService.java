3496c7009c717730b66138cee3b1af2f99338bbe
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-12-07 11:47:19 -0300

    corrigi todos erros

--- src/main/java/projeto_prog_ii/service/ProfessorService.java
+++ src/main/java/projeto_prog_ii/service/ProfessorService.java
@@ -3,13 +3,13 @@ package projeto_prog_ii.service;
 import org.springframework.stereotype.Service;
 import projeto_prog_ii.model.Professor;
 import projeto_prog_ii.repository.ProfessorRepository;
-
 import java.util.List;
+
 @Service
 public class ProfessorService {
 
+    //Injeção e Construtores
     private final ProfessorRepository professorRepository;
-
     public ProfessorService(ProfessorRepository professorRepository) {
         this.professorRepository = professorRepository;
     }
@@ -17,11 +17,12 @@ public class ProfessorService {
     public Professor createProfessores(Professor professor){
         return professorRepository.save(professor);
     }
+    //Owerload
     public Professor readProfessores(Long id) {
         return professorRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
     }
-    public List<Professor> readAllProfessores() {
+    public List<Professor> readProfessores() {
         return professorRepository.findAll();
     }
 
@@ -35,6 +36,7 @@ public class ProfessorService {
 
         return professorRepository.save(existente);
     }
+
     public void delete(Long id){
         professorRepository.deleteById(id);
     }
