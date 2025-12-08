5cda49ec0bcf0ddd9b652fd2a39339bb54743438
lucas-allb <albuquerque.trabalho5625@gmail.com>
2025-12-08 09:09:50 -0300

    Atualização do controller secretaria

--- src/main/java/projeto_prog_ii/controller/SecretariaController.java
+++ src/main/java/projeto_prog_ii/controller/SecretariaController.java
@@ -33,8 +33,8 @@ public class SecretariaController {
     @PostMapping("/atribuir_Prof/{turmaId}/{professorId}")
     public ResponseEntity<Turma> atribuirProfessor(
             @PathVariable long turmaId,
-            @PathVariable long profesorId){
-        return ResponseEntity.ok(secretariaService.atribuirProf(turmaId, profesorId));
+            @PathVariable long professorId){
+        return ResponseEntity.ok(secretariaService.atribuirProf(turmaId, professorId));
     }
 
     @PostMapping("/trocar_Prof/{turmaId}/{professorId}")
